33bd0b2cc395ab2af61b42553ed33838a13bb451
Shui Guo <shui@Shuis-MacBook-Pro.local>
2020-02-17 00:11:54 -0500

    added txt file

---
+++ PyBank/main.py
@@ -0,0 +1,54 @@
+# import library
+import os
+import csv
+# define csv file path
+bank_csv = os.path.join("/Users/shui/Desktop/BSC/UT-TOR-DATA-PT-01-2020-U-C-Master/03-Python/Instructions/python-challenge/PyBank/Resources/budget_data.csv")
+# open and read the csv file line by line
+with open(bank_csv,'r') as csvfile:
+    csvheader = next(csvfile)
+    csvreader = csv.reader(csvfile, delimiter = ",")
+
+# create list variables
+    date = []
+    revenue = []
+    monthly_change = []
+
+# populate the date and revenue lists 
+    for row in csvreader:
+        date.append(row[0])
+        revenue.append(float(row[1]))
+    
+        total_month = len(date)
+        total_revenue = sum(revenue)
+
+# calulate the average change in revenue and profit using the populated lists
+    for i in range(1, total_month):
+        monthly_change.append(revenue[i] - revenue[i-1])
+        average_change = round(sum(monthly_change)/len(monthly_change),2)
+        increase_profit = max(monthly_change)
+        increase_profit_date = str(date[monthly_change.index(max(monthly_change))+1])
+        decrease_profit = min(monthly_change)
+        decrease_profit_date = str(date[monthly_change.index(min(monthly_change))+1])
+
+# print the output on the terminal 
+print(f"Financial Analysis")
+print(f"---------------------------")
+print(f"Total Months: {total_month}")
+print(f"Total: ${total_revenue}")
+print(f"Average Change: ${average_change}")
+print(f"Greatest Increase in Profits: {increase_profit_date} (${increase_profit})")
+print(f"Greatest Decrease in Profits: {decrease_profit_date} (${decrease_profit})")
+
+
+# print the output as a text file
+# alternatively using python main.py >> output.txt in the terminal 
+txtfile_path = "/Users/shui/Desktop/BSC/UT-TOR-DATA-PT-01-2020-U-C-Master/03-Python/Instructions/python-challenge/PyBank/output.txt"
+with open(txtfile_path"w") as txt_file:
+    txt_file.write(f"Financial Analysis\n")
+    txt_file.write(f"---------------------------\n")
+    txt_file.write(f"Total Months: {total_month}\n")
+    txt_file.write(f"Total: ${total_revenue}\n")
+    txt_file.write(f"Average Change: ${average_change}\n")
+    txt_file.write(f"Greatest Increase in Profits: {increase_profit_date} (${increase_profit})\n")
+    txt_file.write(f"Greatest Decrease in Profits: {decrease_profit_date} (${decrease_profit})\n")
+    txt_file.close()
---
+++ PyPoll/main.py
@@ -0,0 +1,94 @@
+# import library
+import os
+import csv
+# define csv file path
+poll_csv = os.path.join("/Users/shui/Desktop/BSC/UT-TOR-DATA-PT-01-2020-U-C-Master/03-Python/Instructions/python-challenge/PyPoll/Resources/election_data.csv")
+# open and read the csv file line by line
+with open(poll_csv,'r') as csvfile:
+    csvheader = next(csvfile)
+    csvreader = csv.reader(csvfile, delimiter = ",")
+# create list variables
+    candidates = []
+# populate the candidates list and find the total votes
+    for row in csvreader:
+        candidates.append(row[2])
+        total_votes = len(candidates)
+
+# print the title block in the terminal
+    print(f"Election Results")
+    print(f"-------------------------")
+    print(f"Total Votes: {total_votes}")
+    print(f"-------------------------")
+
+# build a unordered list of unique items
+    candidate = set(candidates)
+# create a dictionary variable with the candidate names and votes
+    summary_result = {}
+    for i in candidate:
+        summary_result[i] = candidates.count(i)
+# extract the names and votes in the dictionary with the descending order of votes
+    for key, value in sorted(summary_result.items(),key = lambda x: x[1],reverse = True):
+        candidate_vote = value
+        candidate_name = key
+        candidate_vote_percentage = round(value/total_votes*100,2)
+        winning_count = max(summary_result.values())
+        winning_candidate = [key for key, value in summary_result.items() if value == winning_count]
+        print(f"{candidate_name}: {candidate_vote_percentage}% ({candidate_vote})")   
+# print the output in the terminal
+    print(f"--------------------------")
+    print(f"Winner: {winning_candidate[0]}")
+    print(f"--------------------------")
+    
+
+# print the output as a text file
+# alternatively using python main.py >> output.txt in the terminal 
+txtfile_path = "/Users/shui/Desktop/BSC/UT-TOR-DATA-PT-01-2020-U-C-Master/03-Python/Instructions/python-challenge/PyPoll/output.txt"
+with open(txtfile_path,"w") as txt_file:
+    txt_file.write(f"Election Results\n")
+    txt_file.write(f"-------------------------\n")
+    txt_file.write(f"Total Votes: {total_votes}\n")
+    txt_file.write(f"-------------------------\n")
+    for key, value in sorted(summary_result.items(),key = lambda x: x[1],reverse = True):
+        candidate_vote = value
+        candidate_name = key
+        candidate_vote_percentage = round(value/total_votes*100,2)
+        winning_count = max(summary_result.values())
+        winning_candidate = [key for key, value in summary_result.items() if value == winning_count]
+        txt_file.write(f"{candidate_name}: {candidate_vote_percentage}% ({candidate_vote})\n")  
+    txt_file.write(f"--------------------------\n")
+    txt_file.write(f"Winner: {winning_candidate[0]}\n")
+    txt_file.write(f"--------------------------\n")
+#    txt_file.close()
+    
+
+
+
+
+# solution 1
+    #candidate_1_vote = summary_result["Khan"]
+    #candidate_2_vote = summary_result["Correy"]
+    #candidate_3_vote = summary_result["Li"]
+    #candidate_4_vote = summary_result["O'Tooley"]
+
+    #candidate_1_percentage = round(float(candidate_1_vote/total_votes)*100,5)
+    #candidate_2_percentage = round(float(candidate_2_vote/total_votes)*100,5)
+    #candidate_3_percentage = round(float(candidate_3_vote/total_votes)*100,5)
+    #candidate_4_percentage = round(float(candidate_4_vote/total_votes)*100,5)
+
+
+#solution 2
+    #candidate_1_vote = candidates.count("Khan")
+    #candidate_2_vote = candidates.count("Correy")
+    #candidate_3_vote = candidates.count("Li")
+    #candidate_4_vote = candidates.count("O'Tooley")
+
+#solution 3
+    #candidate_1 = [candidate for candidate in candidates if candidate == "Khan"]
+    #candidate_2 = [candidate for candidate in candidates if candidate == "Correy"]
+    #candidate_3 = [candidate for candidate in candidates if candidate == "Li"]
+    #candidate_4 = [candidate for candidate in candidates if candidate == "O'Tooley"]
+
+    #candidate_1_vote = len(candidate_1) 
+    #candidate_2_vote = len(candidate_2)
+    #candidate_3_vote = len(candidate_3)
+    #candidate_4_vote = len(candidate_4)
